cc9426d583d7249fbe962b7f9ffa35facb4f5569
zbf <zbf@timevale.com>
2016-09-19 18:30:53 +0800

    第一次上传

---
+++ first/com/zhaobf/__init__.py
@@ -0,0 +1,66 @@
+count = 100
+print(count)
+
+count = count + 1.0000;
+print(count)
+
+name = "赵佰枫"
+print(name)
+
+flag = True
+print(not flag)
+
+flag = 1
+print(not flag)
+
+flag = 0
+print(not flag)
+
+flag = -1
+print(not not flag)
+
+print(type(flag))
+
+flag = True
+print(type(flag))
+
+name1 = 100;
+del name1;
+name1 = "abcdefghijk"
+print(name1)
+
+print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+
+print(5 + 4)
+print(4.3 + 2)
+print(3 * 7)
+print(3 / 4)
+print(1/3)
+print(4/4)
+print(2 // 4)   # 除法，得到一个整数
+print(9//4)
+print(17 % 2)
+print(2 ** 5)
+
+print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+name1 = "abcde12345"
+print(name1[1:4])
+print(name1[1:-1])
+print(name1[1:])
+print(name1[-1:])
+print(name1[-5:])
+print(name1[5:])
+
+
+print("=================================")
+str = '0123456789'
+print (str[0:3]) #截取第一位到第三位的字符
+print(str[:]) #截取字符串的全部字符
+print (str[6:]) #截取第七个字符到结尾
+print (str[:-3]) #截取从头开始到倒数第三个字符之前
+print (str[2]) #截取第三个字符
+print (str[-1]) #截取倒数第一个字符
+print (str[::-1]) #创造一个与原字符串顺序相反的字符串
+print (str[-3:-1]) #截取倒数第三位与倒数第一位之前的字符
+print (str[-3:]) #截取倒数第三位到结尾
+print (str[:-5:-3]) #逆序截取，具体啥意思没搞明白？
---
+++ first/com/zhaobf/test1.py
@@ -0,0 +1,6 @@
+# 去空格及特殊符号
+s="    abc,   "
+print(s.strip())
+print(s.lstrip())
+print(s.rstrip())
+print(s.rstrip())
