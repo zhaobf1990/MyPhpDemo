bdc13610d709a39a2bcb3fa9d8f1c6e6a1f2341a
zhaobf <375682226@qq.com>
2017-03-06 21:56:47 +0800

    Signed-off-by: zhaobf <375682226@qq.com>

--- first/com/webtest/baidu.py
+++ first/com/webtest/baidu.py
@@ -6,19 +6,19 @@ import sys
 # chromedriver = "C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
 # os.environ["webdriver.chrome.driver"] = chromedriver
 # browser = webdriver.Chrome(chromedriver)
-
-browser=webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe');
-browser.get("http://www.baidu.com")
-time.sleep(2)
 try:
+    browser=webdriver.Chrome(r'C:\Users\feng\AppData\Local\Google\Chrome\Application\chromedriver.exe');
+    browser.get("http://www.baidu.com")
+    time.sleep(2)
+
     # browser.find_element_by_name("tj_login")[1].click();
     browser.find_element_by_xpath('//*[@id="u1"]/a[7]').click();
     time.sleep(2)
-    browser.find_element_by_xpath("//*[@id=\"TANGRAM__PSP_8__userName\"]").send_keys("赵佰枫")
+    browser.find_element_by_xpath("//*[@id=\"TANGRAM__PSP_8__userName\"]").send_keys("�԰۷�")
 except:
     print("Unexpected error:", sys.exc_info()[0])
     raise
-# browser.find_element_by_id("kw").send_keys("赵佰枫")
+# browser.find_element_by_id("kw").send_keys("�԰۷�")
 # browser.find_element_by_id("su").submit()
 
 time.sleep(2)
