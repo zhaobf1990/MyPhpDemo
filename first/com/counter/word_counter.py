ccea4b013397bfd27203762068929a1bf228ff50
zbf <zbf@timevale.com>
2017-10-22 22:46:37 +0800

    统计英文单词的程序

---
+++ first/com/counter/word_counter.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+import collections
+import os
+import re
+
+with open('article') as file1:  ##  打开文本文件
+    str = file1.read().replace("\n", " ")
+    str1 = re.split(' |,|/|-|\.|\)|\(|=|_|<|>|#|\?', str)  # 将文章按照空格划分开
+
+# print "原文本:\n %s" % str1
+
+# print "\n各单词出现的次数：\n %s" % collections.Counter(str1)
+# print collections.Counter(str1)['was']#以字典的形式存储，每个字符对应的键值就是在文本中出现的次数
+
+counter = collections.Counter(str1)
+
+dirs = counter.items()
+
+# sorted(dirs, key=dirs(1), reverse=True)
+
+# print  dirs
+
+result = sorted(dirs, key=lambda item: item[1])
+
+
+result.reverse()
+print  result
